4616e5e7adb0e5b5b175bbee96935db2a0be32a3
Milan Roka <101684566+MilanRoka@users.noreply.github.com>
2023-12-08 16:12:36 +0545

    "updated contact"

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,11 +14,11 @@ const Contact = () => {
             <div className="flex flex-col items-center py-10 text-center sm:flex-row sm:justify-center sm:items-center sm:py-20 text-2xl md:text-3xl lg:text-5xl">
                 <div className="mb-6 sm:mb-0 sm:mr-10 flex items-center flex-col text-center">
                     <FcCellPhone className="text-3xl sm:text-4xl" />
-                    <p className="text-white text-base sm:text-xl">07387 670908</p>
+                    <p className="text-white text-base sm:text-xl">+44 07387 670908</p>
                 </div>
                 <div className="mb-6 sm:mb-0 sm:mr-10 flex items-center flex-col text-center">
                     <RiUserLocationFill className="text-green-100 text-3xl sm:text-4xl" />
-                    <p className="text-white text-base sm:text-xl">Leicester City, England</p>
+                    <p className="text-white text-base sm:text-xl">158 Jarrom Street, Leicester City, England</p>
                 </div>
                 <div className="mb-6 sm:mb-0 sm:mr-10 flex items-center flex-col text-center">
                     <MdAlternateEmail className="text-white text-3xl sm:text-4xl" />
